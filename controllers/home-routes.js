c88d7bb13b87f22606d0da515cd9ca5ccc323cce
Justin Byrd <justin_byrd@hotmail.com>
2021-07-14 19:59:32 -0400

    fixed links for non authorized users on home view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,7 +6,7 @@ const { QueryTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 router.get('/', async (req, res) => {
-	const posts = await sequelize.query(`select a.title, a.body, b.username from post a, user b where a.user_id = b.id`, { type: QueryTypes.SELECT });
+	const posts = await sequelize.query(`select a.id, a.title, a.body, b.username from post a, user b where a.user_id = b.id`, { type: QueryTypes.SELECT });
 	res.render('home', { layout: 'main', isAuthorized: req.session.isAuthenticated, posts: posts});
 });
 
